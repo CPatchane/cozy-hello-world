a79c645ccb7ac913adb5f3e3ac465dcdbb6589d5
CPatchane <code@patchane.com>
2017-10-08 16:29:12 +0200

    chore: remove unused __SENTRY_TOKEN__ :wrench:

--- config/webpack.target.mobile.js
+++ config/webpack.target.mobile.js
@@ -16,8 +16,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       __ALLOW_HTTP__: !production,
-      __TARGET__: JSON.stringify('mobile'),
-      __SENTRY_TOKEN__: JSON.stringify('29bd1255b6d544a1b65435a634c9ff67')
+      __TARGET__: JSON.stringify('mobile')
     }),
     new webpack.ProvidePlugin({
       'cozy.client': 'cozy-client-js/dist/cozy-client.js',
