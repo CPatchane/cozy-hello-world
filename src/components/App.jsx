beee32f282f5be605e4322cc7192087f8afbd3be
CPatchane <code@patchane.com>
2017-10-08 13:50:07 +0200

    fix: move HashRouter usage in App instead of Root component :art:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect, HashRouter } from 'react-router-dom'
 
 import Sidebar from './Sidebar'
 
@@ -10,17 +10,19 @@ const Hello2 = () => Hello('View 2')
 const Hello3 = () => Hello('View 3')
 
 const App = ({ children }) => (
-  <div className='hello-wrapper coz-sticky'>
-    <Sidebar />
-    <main className='hello-content'>
-      <Switch>
-        <Route path='/viewhello1' component={Hello1} />
-        <Route path='/viewhello2' component={Hello2} />
-        <Route path='/viewhello3' component={Hello3} />
-        <Redirect from='/' to='/viewhello1' />
-      </Switch>
-    </main>
-  </div>
+  <HashRouter>
+    <div className='hello-wrapper coz-sticky'>
+      <Sidebar />
+      <main className='hello-content'>
+        <Switch>
+          <Route path='/viewhello1' component={Hello1} />
+          <Route path='/viewhello2' component={Hello2} />
+          <Route path='/viewhello3' component={Hello3} />
+          <Redirect from='/' to='/viewhello1' />
+        </Switch>
+      </main>
+    </div>
+  </HashRouter>
 )
 
 export default App
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { I18n } from 'cozy-ui/react/I18n'
-import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 
 const Root = ({ context, lang, store }) => {
   return <I18n lang={lang} dictRequire={(lang) => require(`../locales/${lang}`)}>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </Provider>
   </I18n>
 }
